eb24e54b227748b4be51afbb8190e690aac6c62d
Sai Tharun Dusa <sd2672@nau.edu>
2025-06-02 01:33:35 -0700

    updated constants keys

---
+++ src/utils/constants.js
@@ -0,0 +1,31 @@
+
+export const BG_PHOTO_URL =
+  "https://assets.nflxext.com/ffe/siteui/vlv3/b2c3e95b-b7b5-4bb7-a883-f4bfc7472fb7/80a8277e-14eb-4192-83f7-45c27cd0652b/US-en-20240805-POP_SIGNUP_TWO_WEEKS-perspective_WEB_99b9a7c9-7791-4a48-b335-09e8ee246500_medium.jpg";
+
+export const USER_AVATAR =
+  "https://occ-0-3997-3996.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABXEGkEDGo2vzJlj1oDachFRFYTAVQRAG_fdkuAWml_xtwVmdX9BoU6GPltBf8HIizsJVJWLcYbQ5k82qdHCdSI-3LBb_Zxk.png?r=b7b";
+
+export const LOGO =
+  "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer " + process.env.REACT_APP_TMDB_KEY ,
+  },
+};
+
+export const TMBD_IMG_URL = "https://image.tmdb.org/t/p/w500";
+
+
+export const SUPPORTED_LANGUAGES = [
+  { identifier: "en", name: "English" },
+  { identifier: "hindi", name: "Hindi" },
+  { identifier: "spanish", name: "Spanish" },
+  { identifier: "french", name: "French"},
+  { identifier: "telugu", name: "Telugu"}
+];
+
+export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY;
